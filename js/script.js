e4357f48468502e4156fb83a6d2464d7b3e5feff
leonardoolia <leonardooliaa@gmail.com>
2023-12-21 15:39:42 +0100

    got html elements

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//? 1. Recuperare gli elementi dall'HTML
+
+const user = document.getElementById('user-dice');
+const pc = document.getElementById('pc-dice');
+const finalResult = document.getElementById('final-result');
+
+
+
