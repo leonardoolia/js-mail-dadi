92bffab141a28f99791b800c321da43e9f8c3fbd
leonardoolia <leonardooliaa@gmail.com>
2023-12-21 15:40:22 +0100

    created random numbers

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ const pc = document.getElementById('pc-dice');
 const finalResult = document.getElementById('final-result');
 
 
+//? 2. Creare variabili per estrazione casuale di numeri da 1 a 6
+
+// Dado dell'utente
+const userDice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+console.log('Dado utente: ', userDice);
+
+// Dado del pc
+const pcDice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+console.log('Dado pc: ', pcDice);
+
+
 
